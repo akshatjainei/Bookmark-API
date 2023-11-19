d25b3c3406ad0862278a5cd98c5f0c5dc1539d7c
akshatjainei <jainakshat447@gmail.com>
2023-11-19 11:46:08 +0530

    DTO logic and pipes added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post , Req , Body, ParseBoolPipe, ParseIntPipe} from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+import { error } from "console";
 
 @Controller('auth')
 export class AuthController{
     constructor(private authService : AuthService){}
      
     @Post('signin')
-    signin(){
+    signin(@Body() dto : AuthDto){
+        console.log({
+            email : dto.email,
+            password : dto.password
+        });
         return this.authService.signin()
+    }
+
+    @Post('signup')
+    signup(){
+        return this.authService.signup()
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})
 export class AuthService {
+    constructor(private prisma : PrismaService){}
     signin(){
         return {msg : 'Hello World'}
+    }
+
+    signup(){
+        return {msg : 'Lmao'}
     }
 }
